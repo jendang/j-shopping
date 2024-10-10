bfea8497ba790172dc3789af8004885dc881ce37
jendang <jenny.dang117@gmail.com>
2024-10-10 14:56:28 +0200

    write data to firestore and get data from firestore

--- src/context/product.context.jsx
+++ src/context/product.context.jsx
@@ -1,13 +1,43 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+//import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
+//import SHOP_DATA  from '../shop-data.js'
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
-import PRODUCTS from '../shop-data.json'
-
+/*
+    // SAMPLE DATA STRUCTURE
+    hats:{
+        title: "Hats"
+        items: [
+            {
+                id,
+                name, 
+                price,
+            },
+            {},
+        ]
+    }
+*/
 export const ProductsContext = createContext({
     products: []
 });
 
 export const ProductsProvider = ({ children }) => {
-    const [ products, setProducts ] = useState(PRODUCTS)
+    const [ products, setProducts ] = useState([])
+
+    // *** RUN ONE TIME TO WRITE DATA INTO FIRESTORE DATABASE
+    // useEffect(() => {
+    //     addCollectionAndDocuments("categories", SHOP_DATA);
+    // }, [])
+
+    useEffect(() => {
+        const getCategoriesMap = async() => {
+            const categoriesMap = await getCategoriesAndDocuments();
+            console.log(categoriesMap);
+        }
+
+
+        getCategoriesMap();
+    }, [])
 
     const value = { products }
 
