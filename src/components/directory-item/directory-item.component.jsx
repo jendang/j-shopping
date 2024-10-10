1a23ff1c31e71e087e81246d5b7672a88047d0cb
jendang <jenny.dang117@gmail.com>
2024-10-10 16:10:32 +0200

    add category page for each category

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,14 +1,14 @@
-import "./category-item.styles.scss"
+import "./directory-item.styles.scss"
 
-const CategoryItem = ({ title, imageUrl }) => {
+const DirectoryItem = ({ title, imageUrl }) => {
     return(
-        <div className="category-container">
+        <div className="directory-container">
             <div 
             className="background-image" 
             style={{ 
             backgroundImage: `url(${imageUrl})`,
             }}></div>
-            <div className="category-body-container">
+            <div className="directory-body-container">
                 <h2>{title}</h2>
                 <p>Shop now</p>
             </div>
@@ -16,4 +16,4 @@ const CategoryItem = ({ title, imageUrl }) => {
     )
 }
 
-export default CategoryItem;
+export default DirectoryItem;
